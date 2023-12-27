01fe498bfe354f359e63c9c3e2fc385049c6e619
Surajit Sadhukhan <surajit1992@live.com>
2023-12-27 08:12:43 +0530

    update

--- src/Config/constant.js
+++ src/Config/constant.js
@@ -1,4 +1,5 @@
 export const PRODUCTION_URL = 'https://mrovpnawkyiutwl3jrxswrhgre0nlslp.lambda-url.us-west-2.on.aws'
 
+
 export const SET_USER_TOKEN = 'SET_USER_TOKEN'
 export const SAVE_MACHINE_DETAILS = 'SAVE_MACHINE_DETAILS'
