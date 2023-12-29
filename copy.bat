da24f1004e47a890c81aaf52ed83a1f57aa0926f
Surajit Sadhukhan <surajit1992@live.com>
2023-12-29 09:57:05 +0530

    ggfggf

--- copy.bat
+++ copy.bat
@@ -1,11 +1,12 @@
 whoami
-# yarn install
-# yarn run build
-# yarn run stop-server
-# yarn run run-server
-# yarn run serve
+#yarn install
+#yarn run build
+#yarn run stop-server
+#yarn run run-server
+#yarn run serve
 
 yarn install
 yarn run build
-rmdir C:\Users\Administrator\OneDrive\Windows\Jenkins\.jenkins\xampp\htdocs\build
+Remove-Item C:\Users\Administrator\OneDrive\Windows\Jenkins\.jenkins\xampp\htdocs\build
+Remove-Item ../../../xampp/htdocs/build
 move ./build ../../../xampp/htdocs
