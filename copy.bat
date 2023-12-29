865e943cf5cb7c37b7878224f4a1419be1eab90f
Surajit Sadhukhan <surajit1992@live.com>
2023-12-29 10:03:32 +0530

    update

--- copy.bat
+++ copy.bat
@@ -7,6 +7,16 @@ whoami
 
 yarn install
 yarn run build
-Remove-Item C:\Users\Administrator\OneDrive\Windows\Jenkins\.jenkins\xampp\htdocs\build -recurse -force
-Remove-Item ../../../xampp/htdocs/build -recurse -force
+#Remove-Item C:\Users\Administrator\OneDrive\Windows\Jenkins\.jenkins\xampp\htdocs\build -recurse -force
+#Remove-Item ../../../xampp/htdocs/build -recurse -force
+$FolderName = "../../../xampp/htdocs/build"
+if (Test-Path $FolderName) {
+ 
+    Write-Host "Folder Exists"
+    Remove-Item $FolderName -Recurse -Force
+}
+else
+{
+    Write-Host "Folder Doesn't Exists"
+}
 move ./build ../../../xampp/htdocs
