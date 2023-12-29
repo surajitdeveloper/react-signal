62f53de3ae3ffcf9e4cec3b585d50fe8ca8c4d68
Surajit Sadhukhan <surajit1992@live.com>
2023-12-29 09:50:55 +0530

    bat

--- copy.bat
+++ copy.bat
@@ -1,11 +1,11 @@
 whoami
-yarn install
-yarn run build
-yarn run stop-server
-yarn run run-server
-yarn run serve
+# yarn install
+# yarn run build
+# yarn run stop-server
+# yarn run run-server
+# yarn run serve
 
 yarn install
 yarn run build
-rmdir C:/Users/Administrator/OneDrive/Windows/Jenkins/.jenkins/xampp/htdocs/build
+rmdir C:\Users\Administrator\OneDrive\Windows\Jenkins\.jenkins\xampp\htdocs\build
 move ./build ../../../xampp/htdocs
