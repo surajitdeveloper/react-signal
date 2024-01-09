63db25a01ce4ee65ec30d74613ab6fc355ac1468
Surajit Sadhukhan <surajit1992@live.com>
2024-01-09 09:50:37 +0530

    update api

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -10,15 +10,10 @@ export const todoList = async () => {
 }
 
 axios.interceptors.request.use(function (config) {
-  const token = store.getState().token;
-  if(token){
-    config.headers.Authorization =  `Bearer ${token}`;
-    return config;
-  }
-  else{
-    return config;
-  }
-});
+  const token = store.getState().token
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
 
 export const api = async (data) => {
   // console.log(data)
@@ -26,6 +21,10 @@ export const api = async (data) => {
   switch (METHOD) {
     case 'post':
       return (await axios.post(`${PRODUCTION_URL}/${PATH}`, PAYLOAD)).data
+    case 'put':
+      return (await axios.put(`${PRODUCTION_URL}/${PATH}`, PAYLOAD)).data
+    case 'delete':
+      return (await axios.delete(`${PRODUCTION_URL}/${PATH}`, PAYLOAD)).data
     case 'get':
       return (await axios.get(`${PRODUCTION_URL}/${PATH}`)).data
     default:
